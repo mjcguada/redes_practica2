a84ec60a9903b7d193048ed091a0e917f2347049
unknown <josemonr@ucm.es>
2018-04-19 22:01:21 +0200

    Ejercicio 5 subido

--- Practica 2.1/P2_E5.cc
+++ Practica 2.1/P2_E5.cc
@@ -1,17 +1,28 @@
 #include <time.h>
 #include <stdio.h>
 #include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <math.h>
 
 int main (void)
 {
-   	time_t tiempo;
-   	struct tm *ti_legible;
- 
-   	//Getting current time of system
-   	tiempo = time (NULL); 
-	int cosa = 0;
-	for(int i = 0; i < 10000; i++)	
-		cosa++;	
+	timespec tiempo1;
+	timespec tiempo2;
+	long t1, t2;
 
-  return 0;
+	t1 = clock_gettime(2, &tiempo1);
+	if (t1 != 0) {
+		perror("Error variable 1");
+	}
+	for (int i = 0; i < 10000; i++) {
+
+	}
+	t2 = clock_gettime(2, &tiempo2);
+
+	std::cout << "Tiempo: " << (long)pow(tiempo2.tv_sec,3) + tiempo2.tv_nsec << "(ns)" << std::endl;
+
+	return 1;
 }
