82002a2e3613e1af334d16090456f8786e484fdf
JoseInside <josemonr@ucm.es>
2018-05-24 21:29:00 +0200

    UDPServer casi acabado

--- practica2.3/replicacion/Socket.cc
+++ practica2.3/replicacion/Socket.cc
@@ -33,23 +33,29 @@ std::ostream& operator<<(std::ostream& os, const Socket& s)
 Socket::Socket(const char * address, const char * port):sd(-1)
 {
 	struct addrinfo hints;
+	struct addrinfo aux;
 
 	memset(&hints, 0, sizeof(struct addrinfo)); 
 
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
+	
 
-	int err = getaddrinfo(address, port, &hints, (const struct sockaddr *) &sa);
+	int err = getaddrinfo(address, port, &hints, (const struct sockaddr *) &aux);
 
 	if(err != 0){
 		printf("Error en getaddrinfo \n");
-	}
-
-	sd = socket(sa->ai_family, sa->ai_socktype,0);
+	}	
+	
+	sd = socket(aux->ai_family, aux->ai_socktype, 0);
 
 	if (sfd == -1){
 		printf("Error en creacion del socket \n");
 	}
+
+	//Pasamos los datos al socket en sí (nos faltaba esto)
+	sa = aux->ai_addr;
+	sa_len = aux->ai_addrlen;
 }
 
 // ----------------------------------------------------------------------------
@@ -63,19 +69,30 @@ int Socket::bind()
 
 int Socket::send(Serializable * obj, Socket * sock)
 {
+	obj->to_bin();
 	
+	//No sé si hace falta el (void*)
+	int result =  sendto(sock->sd, (void*)obj->_data, obj->_size, 0, &sock->sa,sock->sa_len);
+
+	if(result != 0)
+		return -1;
+	else return 0;
 }
 
 // ----------------------------------------------------------------------------
 
 int Socket::recv(char * buffer, Socket ** sock)
 {
-	//char buf [MAX_MESSAGE_SIZE];
-	
-	buffer->to_bin();
+	//char buff [MAX_MESSAGE_SIZE];
+	struct sockaddr sck_addr;
+	socklen_t len = sizeof(sck_addr);
+	//buffer->to_bin();
 
 	if(sock != 0){
-		int result =  socket(sa, sa_len,0);
+		//No sé si hace falta el (void*)
+		int result =  recvfrom(sd, (void*)&buffer, MAX_MESSAGE_SIZE, 0, &sck_addr, &len);
+		
+		sock = new Socket(&src,port);		
 	}
 
 	if(result != 0)
--- practica2.3/replicacion/UDPServer.cc
+++ practica2.3/replicacion/UDPServer.cc
@@ -27,24 +27,95 @@ extern "C" void * _server_thread(void *arg)
 
 int UDPServer::start()
 {
+	//Creamos los threads que gestionarán los mensajes 
+	pthread_t pThreads[THREAD_POOL_SIZE];
+	
+	for(int i = 0; i < THREAD_POOL_SIZE; i++){
+
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		//pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+		int newThread = pthread_create(&pThreads[i], &attr, _server_thread, static_cast<void*>(this));
+
+		if(newThread != 0){		
+			printf("ERROR: Thread no creado \n");
+			return -1;
+		}
+
+	}
+
+	return 0;
 }
 
 // ----------------------------------------------------------------------------
 
 void UDPServer::server_thread()
 {
+	while(true){
+
+		char* buff;
+		struct sockaddr sock;
+		//socklen_t tam = sizeof(sock);
+
+		Socket* newSocket;
+		socket.recv(buff,&newSocket);
+		add_connection(newSocket);
+		do_message(buff);
+	}
 }
 
 // ----------------------------------------------------------------------------
 
 void UDPServer::add_connection (Socket * s)
 {
+	//BLOQUEO
+	pthread_mutex_lock(&mutex);
+
+	bool found = false;
+
+	for(Socket* aux : connections){
+
+		if(s == aux){
+			//Si existe la borramos
+			found = true;
+			delete s; 
+		}
+
+	}
+
+	if(!found && connections.size() < THREAD_POOL_SIZE)
+		connections.push_back(s);
+	
+	//DESBLOQUEO
+	pthread_mutex_unlock(&mutex);
 }
 
 // ----------------------------------------------------------------------------
 
 void UDPServer::del_connection (Socket * s)
 {
+	pthread_mutex_lock(&mutex);
+
+	bool found = false;
+	
+	//Necesitamos este iterador para avanzar por la lista de conexiones
+	std::vector<Socket *>::iterator iterador = connections.begin();
+
+	//Buscamos la conexión a quitar hasta llegar al final
+	while (!found && iterador != connections.end()){
+		
+		if(iterador == s){
+			found = true;
+			connections.erase(iterador);
+		}
+	}
+	
+	//Si no existe la conexión, borramos el socket
+	if(!found)
+		delete s;
+
+	pthread_mutex_unlock(&mutex);
 }
 
 // ----------------------------------------------------------------------------
